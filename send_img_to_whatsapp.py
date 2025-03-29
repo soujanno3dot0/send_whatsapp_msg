889c01ddd6871dacad9025eb6aa9f9166cec97e2
Sou <53225472+soujanno3dot0@users.noreply.github.com>
2025-03-29 12:41:05 +0530

    Add files via upload

---
+++ send_img_to_whatsapp.py
@@ -0,0 +1,8 @@
+# Send Coupon images
+
+import pywhatkit
+
+#img = r"C:\Users\Sana\Desktop\dance.jpg"
+
+pywhatkit.sendwhats_image('+918095820294',r'C:\Docs\Projects\send_whatsapp_msg\coupons\AUMSUM .jpg',"",20,True,3)
+print("sent..")
